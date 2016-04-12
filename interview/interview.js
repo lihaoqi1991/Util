a499ec1d5e2365c3c9f1c0f02ded254c9751556a
lihaoqi1991 <lihaoqi1991@126.com>
2016-04-12 17:49:38 +0800

    interview

--- interview/interview.js
+++ interview/interview.js
@@ -1,14 +1,17 @@
 /**
  * Created by lihaoqi on 16/3/23.
  */
+var foo = {n: 1};
+var bar = foo;
+foo.x = foo = {n: 2};
 
-function Foo(y){
+function Foo(y) {
     this.y = y;
 }
 
 Foo.prototype.x = 10;
-Foo.prototype.calculate = function(z){
-    return this.x + this.y +z;
+Foo.prototype.calculate = function (z) {
+    return this.x + this.y + z;
 };
 
 var b = new Foo(20);
@@ -34,13 +37,13 @@ console.log(10 - "5");
 
 console.log("********************************************");
 
-var Test = function(num){
+var Test = function (num) {
     this._mynum = 10;
-    var funA = function(){
+    var funA = function () {
         console.log(num);
         console.log(this._mynum);
     };
-    this.funB = function(){
+    this.funB = function () {
         funA();
     };
     this.funC = funA;
@@ -49,4 +52,34 @@ var Test = function(num){
 var a = new Test(5);
 a.funB();
 a.funC();
+
+function Person() {
+    if (!(this instanceof Person)) {
+        return new Person();
+    }
+    this.a = 3;
+    this.b = 4;
+}
+var person1 = new Person();
+var person2 = Person();
+
+"I ma a lasagna hog".split("").reverse().join("");
+
+var foo = "hello";
+(function () {
+    var bar = " World";
+    alert(foo + bar);
+})();
+alert(foo + bar);
+
+var foo = {n: 1};
+var bar = foo;
+foo.x = foo = {n: 2};
+
+console.log('one');
+setTimeout(function () {
+    console.log('two');
+}, 0);
+console.log('three');
+
 
