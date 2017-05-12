bc024b57be4f1924154a6641daeb6c676cedb761
lihaoqi1991 <lihaoqi1991@126.com>
2017-05-12 11:02:58 +0800

    interview

--- interview/interview.js
+++ interview/interview.js
@@ -30,6 +30,27 @@ console.log(
     b.__proto__.calculate === Foo.prototype.calculate
 );
 
+
+(function() {
+    var $w = $(window);
+    var $prog2 = $('.progress-indicator-2');
+    var wh = $w.height();
+    var h = $('body').height();
+    var sHeight = h - wh;
+    $w.on('scroll', function() {
+        window.requestAnimationFrame(function(){
+            var perc = Math.max(0, Math.min(1, $w.scrollTop() / sHeight));
+            updateProgress(perc);
+        });
+    });
+
+    function updateProgress(perc) {
+        $prog2.css({width: perc * 100 + '%'});
+        ditto.save_progress && store.set('page-progress', perc);
+    }
+
+}());
+
 console.log("********************************************");
 
 console.log(10 + "5");
